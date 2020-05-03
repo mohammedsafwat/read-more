926bb9fc4b0fa309856f662ebb575be525742b2c
Mohammed Abdullatif <mabdullatif@spotify.com>
2020-05-03 18:41:40 +0200

    Update the subtitle text.

--- ReadMore/ContentView.swift
+++ ReadMore/ContentView.swift
@@ -183,7 +183,7 @@ struct BottomCardView: View {
                 RingView(color1: Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)), color2: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)), width: 88, height: 88, percent: 78, show: $showRingView)
                 VStack(alignment: .leading, spacing: 8.0) {
                     Text("Swift Testing").fontWeight(.bold)
-                    Text("12 of 12 sections completed\n10 hours spent so far")
+                    Text("12 of 12 sections completed.\n10 hours spent so far.")
                         .font(.footnote)
                         .foregroundColor(.gray)
                         .lineSpacing(4)
