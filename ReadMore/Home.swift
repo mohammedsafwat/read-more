cd4e4de8b67e089d6d3d85c8c6b99765d4bf802d
Mohammed Abdullatif <mabdullatif@spotify.com>
2020-05-03 23:05:49 +0200

    Add a gradient background Color. Display the library when the user taps on the WatchRingViews.

--- ReadMore/Home.swift
+++ ReadMore/Home.swift
@@ -11,12 +11,23 @@ import SwiftUI
 struct Home: View {
     @State var showProfile = false
     @State var viewState = CGSize.zero
+    @State var showContent = false
 
     var body: some View {
         ZStack {
-            HomeView(showProfile: $showProfile)
-                .padding(.top)
-                .background(Color.white)
+            Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+                .edgesIgnoringSafeArea(.all)
+
+            HomeView(showProfile: $showProfile, showContent: $showContent)
+                .padding(.top, 44)
+                .background(
+                    VStack {
+                        LinearGradient(gradient: Gradient(colors: [Color("background2"), .white]), startPoint: .top, endPoint: .bottom)
+                            .frame(height: 200)
+                        Spacer()
+                    }
+                    .background(Color.white)
+                )
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                 .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
                 .offset(y: showProfile ? -450 : 0)
@@ -43,6 +54,31 @@ struct Home: View {
                     self.viewState = .zero
                 }
             )
+
+            if showContent {
+                Color.white.edgesIgnoringSafeArea(.all)
+
+                ContentView()
+
+                VStack {
+                    HStack {
+                        Spacer()
+
+                        Image(systemName: "xmark")
+                            .frame(width: 36, height: 36)
+                            .foregroundColor(.white)
+                            .background(Color.black)
+                            .clipShape(Circle())
+                    }
+                    Spacer()
+                }
+                .offset(x: -16, y: 16)
+                .transition(.move(edge: .top))
+                .animation(.spring(response: 0.6, dampingFraction: 0.8, blendDuration: 0))
+                .onTapGesture {
+                    self.showContent = false
+                }
+            }
         }
     }
 }
