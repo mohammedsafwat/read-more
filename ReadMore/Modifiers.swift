b8e942e47950b5566cfa4b1a67a643ec19d1c638
Mohammed Abdullatif <mabdullatif@spotify.com>
2020-05-03 18:40:59 +0200

    Add "Work_Sans" font family.

---
+++ ReadMore/Modifiers.swift
@@ -0,0 +1,21 @@
+//
+//  Modifiers.swift
+//  ReadMore
+//
+//  Created by Mohammed Abdullatif on 2020-05-03.
+//  Copyright © 2020 Mohammed Abdullatif. All rights reserved.
+//
+
+import SwiftUI
+
+struct Modifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Modifiers_Previews: PreviewProvider {
+    static var previews: some View {
+        Modifiers()
+    }
+}
