84eda29a573cec02054eaf240ad8d19d0d16d464
Mohammed Abdullatif <mabdullatif@spotify.com>
2020-07-04 01:20:37 +0200

    Update the BookView component to include description texts in a ZStack.

--- ReadMore/BookList.swift
+++ ReadMore/BookList.swift
@@ -26,35 +26,62 @@ struct BookView: View {
     @State var show = false
 
     var body: some View {
-        VStack {
-            HStack(alignment: .top) {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Pro Swift")
-                        .font(.system(size: 24, weight: .bold))
-                        .foregroundColor(.white)
-                    Text("20 Chapters")
-                        .foregroundColor(Color.white.opacity(0.7))
+        ZStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 30) {
+                Text("The famous book by Paul Hudson about mastering Swift skills.")
+                Text("About this book").font(.title).bold()
+                Text("Composed of 20 chapters, this book takes you on a journey to master your Swift skills.")
+            }
+            .padding(30)
+            .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight:  show ? .infinity : 280, alignment: .top)
+            .offset(y: show ? 460 : 0)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .opacity(show ? 1 : 0)
+
+            VStack {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Pro Swift")
+                            .font(.system(size: 24, weight: .bold))
+                            .foregroundColor(.white)
+                        Text("20 Chapters")
+                            .foregroundColor(Color.white.opacity(0.7))
+                    }
+                    Spacer()
+                    ZStack {
+                        Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+                            .opacity(show ? 0 : 1)
+                        VStack {
+                            Image(systemName: "xmark")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(.white)
+                        }
+                        .frame(width: 36, height: 36)
+                        .background(Color(.black))
+                        .clipShape(Circle())
+                        .opacity(show ? 1 : 0)
+                    }
                 }
                 Spacer()
-                Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+                Image(uiImage: #imageLiteral(resourceName: "Card4"))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 140, alignment: .top)
             }
-            Spacer()
-            Image(uiImage: #imageLiteral(resourceName: "Card4"))
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(maxWidth: .infinity)
-                .frame(height: 140, alignment: .top)
+            .padding(show ? 30 : 20)
+            .padding(.top, show ? 30 : 0)
+            .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? 460 : 280)
+            .background(Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)).opacity(0.5), radius: 20, x: 0, y: 20)
+            .onTapGesture {
+                self.show.toggle()
+            }
         }
-        .padding(show ? 30 : 20)
-        .padding(.top, show ? 30 : 0)
-        .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? .infinity : 280)
-        .background(Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)))
-        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-        .shadow(color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)).opacity(0.5), radius: 20, x: 0, y: 20)
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-        .onTapGesture {
-            self.show.toggle()
-        }
         .edgesIgnoringSafeArea(.all)
     }
 }
